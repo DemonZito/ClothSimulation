cef72ab3e65210545753b6452cb01b2a764db822
prunefriend <jwjmair@gmail.com>
2018-06-05 19:10:31 +1200

    FIXED lighting

--- OpenGL-May-Submission-2/OpenGL May Submission/fragment_shader.glsl
+++ OpenGL-May-Submission-2/OpenGL May Submission/fragment_shader.glsl
@@ -7,8 +7,6 @@ in vec3 outNormal;
 
 void main(void)
 {
-	//gl_FragColor = vec4(outColor, 1.0f);
-
 	float ambientIntensity = 0.8f;
 
 
@@ -21,17 +19,18 @@ void main(void)
 	float diff = max(dot(lightDir, norm), 0.0);
 
 	//Toon shader stuff
-	//if (diff > 0.75f)
-	//	diff = 1;
-	//else if (diff > 0.5f)
-	//	diff = 0.5f;
-	//else if (diff > 0.25f)
-	//	diff = 0.25f;
-	//else
-	//	diff = 0;
+		//if (diff > 0.75f)
+		//	diff = 1;
+		//else if (diff > 0.5f)
+		//	diff = 0.5f;
+		//else if (diff > 0.25f)
+		//	diff = 0.25f;
+		//else
+		//	diff = 0;
 
 	vec3 ambient = ambientIntensity * outColor;
-	vec3 diffuse = diff * outColor;
+
+	vec3 diffuse =  vec3((1- diff) * 0.8, 0.1, diff * 0.8)  * outNormal * outColor;
 
 	//Apply lighting stuff
 	gl_FragColor = vec4(ambient + diffuse, 1.0);
---
+++ ReadMe.txt
@@ -0,0 +1,31 @@
+Controls:
+
+	WASD : 	Move Camera
+
+Wind Direction:
+	North East : 	T
+	North : 	Y
+	North West : 	U
+	West : 		G
+	East : 		J
+	South West : 	B
+	South : 	N
+	South East : 	M
+	Reset : 	H
+
+
+Wind Strength:
+	Decrease : K
+	Increase : L
+
+Cloth Width:
+	Decrease : 1
+	Increase : 2
+
+Cloth Length:
+	Decrease : 3
+	Increase : 4
+
+Hooks
+	Add : 5
+	Subtract : 6
